ff3a8e1d3fc41dde02884c147c44f9605d99c9d0
Wellee11 <68993490+Wellee11@users.noreply.github.com>
2020-10-11 20:09:48 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,31 @@
+// Ultrasonic Sensor
+#define ULTRASONIC_INTERVALL 15
+#define TRIGGER 23
+#define ECHO 25
+int distance;
+int duration;
+
+// Bluetooth data
+int prevThrottle = 49;
+int prevSteering = 49;
+int throttle, steering, sliderVal, sliderId, button;
+int select_drivemode = 3; // starting in steering mode
+
+
+// Engines
+#define VELOCITY_L 3
+#define ROTATIONDIR_L1 2
+#define ROTATIONDIR_L2 4
+#define ROTATIONDIR_R1 7
+#define ROTATIONDIR_R2 8 
+#define VELOCITY_R 9
+
+// Velocities
+int velo_frontal = 80 + 175/2;
+int velo_spin = 115;
+int velo_slowturn;
+
+// Servo
+#define SERVO_INTERVALL 15
+
+#define BAUDRATE 9600
